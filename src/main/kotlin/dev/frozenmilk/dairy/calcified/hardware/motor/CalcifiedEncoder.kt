78115e3ced08859ef9cf96e25e107d24a4960dbf
Smartyguy001 <109079347+Smartyguy001@users.noreply.github.com>
2024-01-14 23:42:20 +0200

    Filtered and unfiltered velocity for TicksEncoder

--- src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/motor/CalcifiedEncoder.kt
+++ src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/motor/CalcifiedEncoder.kt
@@ -33,63 +33,83 @@ abstract class CalcifiedEncoder<T> internal constructor(val module: CalcifiedMod
 }
 
 class TicksEncoder internal constructor(module: CalcifiedModule, port: Byte) : CalcifiedEncoder<Int>(module, port) {
-	override val positionSupplier: CachedCompoundSupplier<Int, Double> = object : CachedCompoundSupplier<Int, Double> {
-		private var cachedPosition: Int? = null
-		private var cachedError: Double? = null
+    override val positionSupplier: CachedCompoundSupplier<Int, Double> = object :
+        CachedCompoundSupplier<Int, Double> {
+        private var cachedPosition: Int? = null
+        private var cachedError: Double? = null
 
-		/**
-		 * returns error in ticks, consider wrapping this encoder in a different UnitEncoder to use error with some other, more predictable unit
-		 */
-		override fun findError(target: Int): Double {
-			if (cachedError == null) cachedError = (target - get()).toDouble()
-			return cachedError!!
-		}
+        /**
+         * returns error in ticks, consider wrapping this encoder in a different UnitEncoder to use error with some other, more predictable unit
+         */
+        override fun findError(target: Int): Double {
+            if (cachedError == null) cachedError = (target - get()).toDouble()
+            return cachedError!!
+        }
 
-		override fun clearCache() {
-			cachedError = null
-			cachedPosition = null
-		}
+        override fun clearCache() {
+            cachedError = null
+            cachedPosition = null
+        }
 
-		override fun get(): Int {
-			if (cachedPosition == null) cachedPosition = module.bulkData.getEncoder(port.toInt()) * direction.multiplier
-			return cachedPosition!! - offset
-		}
-	}
+        override fun get(): Int {
+            if (cachedPosition == null) cachedPosition = module.bulkData.getEncoder(port.toInt()) * direction.multiplier
+            return cachedPosition!! - offset
+        }
+    }
 
-	override val velocitySupplier: CachedCompoundSupplier<Double, Double> = object : CachedCompoundSupplier<Double, Double> {
-		private var cachedVelocity: Double? = null
-		private var cachedError: Double? = null
-		private var previousPosition = positionSupplier.get()
+    override val velocitySupplier: CachedCompoundSupplier<Double, Double> = object :
+        CachedCompoundSupplier<Double, Double> {
+        private var cachedVelocity: Double? = null
+        private var cachedRawVelocity: Double? = null
+        private var cachedError: Double? = null
+        private var previousPositions = ArrayDeque(listOf(Pair(module.cachedTime, positionSupplier.get())))
 
-		override fun findError(target: Double): Double {
-			if (cachedError == null) cachedError = target - get()
-			return cachedError!!
-		}
+        override fun findError(target: Double): Double {
+            if (cachedError == null) cachedError = target - get()
+            return cachedError!!
+        }
 
-		override fun clearCache() {
-			previousPosition = positionSupplier.get()
-			cachedError = null
-			cachedVelocity = null
-		}
+        override fun clearCache() {
+            while (previousPositions.size >= 2 && module.cachedTime - previousPositions[1].first >= velocityTimeWindow) {
+                previousPositions.removeFirst()
+            }
+            previousPositions.addLast(Pair(module.cachedTime, positionSupplier.get()))
+            cachedError = null
+            cachedVelocity = null
+            cachedRawVelocity = null
+        }
 
-		/**
-		 * the velocity since the last time this method was called
-		 */
-		override fun get(): Double {
-			if (cachedVelocity == null) {
-				cachedVelocity = (positionSupplier.get() - previousPosition).toDouble() / (module.cachedTime - module.previousCachedTime)
-			}
-			return cachedVelocity!!
-		}
-	}
-	override var offset: Int = 0
-	override var position: Int
-		get() {
-			return positionSupplier.get()
-		}
-		set(value) {
-			offset = value - positionSupplier.get()
-		}
+        /**
+         * the velocity since the last time this method was called
+         */
+        override fun get(): Double {
+            if (cachedVelocity == null) {
+                while (previousPositions.size >= 2 && module.cachedTime - previousPositions[1].first >= velocityTimeWindow) {
+                    previousPositions.removeFirst()
+                }
+                cachedVelocity = (positionSupplier.get() - previousPositions[0].second).toDouble() / (module.cachedTime - previousPositions[0].first)
+                cachedRawVelocity = (positionSupplier.get() - previousPositions.last().second).toDouble() / (module.cachedTime - previousPositions.last().first)
+            }
+            return cachedVelocity!!
+        }
+
+        /**
+         * the unfiltered velocity since the last time this method was called
+         */
+        fun getRaw(): Double {
+            get()
+            return cachedRawVelocity!!
+        }
+    }
+    override var offset: Int = 0
+    override var position: Int
+        get() {
+            return positionSupplier.get()
+        }
+        set(value) {
+            offset = value - positionSupplier.get()
+        }
+    var velocityTimeWindow: Double = 0.0
 }
 
 abstract class UnitEncoder<T>(private val ticksEncoder: TicksEncoder, protected val ticksPerUnit: Double) : CalcifiedEncoder<T>(ticksEncoder.module, ticksEncoder.port) {
